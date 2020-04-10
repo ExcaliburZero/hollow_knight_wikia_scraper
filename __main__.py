ca660bfe4ed19bf77ae1926464bb309b61df8b60
Christopher Wells <cwellsny@nycap.rr.com>
2020-04-09 23:51:39 -0700

    Write downloaded html pages

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from typing import List, IO, Set, Optional
 
+import abc
 import argparse
+import os
 import sys
 import urllib.parse
 
@@ -14,13 +16,14 @@ def main(argv: List[str]) -> None:
 
     parser.add_argument("start_page")
     parser.add_argument("--max_num_pages", type=int, default=None)
+    parser.add_argument("--page_html_dir", default="page_html")
 
     args = parser.parse_args(argv)
 
     print(args)
 
     config = Config.from_args(args)
-    io_manager = IOManager.default_streams()
+    io_manager = IOManager.default_streams(config)
 
     if not config.validate(io_manager):
         print("Invalid flags")
@@ -33,6 +36,7 @@ def main(argv: List[str]) -> None:
 class Config:
     wiki_name: str
     start_page: str
+    page_html_dir: str
     max_num_pages: Optional[int]
 
     def validate(self, io_manager: "IOManager") -> bool:
@@ -56,22 +60,44 @@ class Config:
         return Config(
             wiki_name="HollowKnight",
             start_page=args.start_page,
+            page_html_dir=args.page_html_dir,
             max_num_pages=args.max_num_pages,
         )
+
+
+class PageWriter(abc.ABC):
+    @abc.abstractmethod
+    def write_html(self, page_name: str, html: str) -> str:
+        raise NotImplementedError
+
+
+@dataclass
+class FilesystemPageWriter(PageWriter):
+    html_dir: str
+
+    def write_html(self, page_name: str, html: str) -> str:
+        filepath = os.path.join(self.html_dir, "{}.html".format(page_name))
+
+        os.makedirs(self.html_dir, exist_ok=True)
+
+        with open(filepath, "w") as output_stream:
+            output_stream.write(html)
+
+        return filepath
 
 
 @dataclass
 class IOManager:
     output_stream: IO[str]
     error_stream: IO[str]
-    html_writer: None  # TODO: implement
+    html_writer: PageWriter
 
     @staticmethod
-    def default_streams() -> "IOManager":
+    def default_streams(config: Config) -> "IOManager":
         return IOManager(
             output_stream=sys.stdout,
             error_stream=sys.stderr,
-            html_writer=None,  # TODO: implement
+            html_writer=FilesystemPageWriter(config.page_html_dir),
         )
 
 
@@ -79,7 +105,7 @@ class IOManager:
 class Page:
     name: str
     outgoing_links: Set[str]
-    html: str
+    html_path: str
 
 
 def run(config: Config, io_manager: "IOManager") -> None:
@@ -116,7 +142,7 @@ def recursively_download_pages(
 
         page_name = pages_to_download.pop()
 
-        page = download_page(config, page_name)
+        page = download_page(config, io_manager, page_name)
 
         downloaded_pages.append(page)
         downloaded_page_names.add(page_name)
@@ -128,19 +154,16 @@ def recursively_download_pages(
     return downloaded_pages
 
 
-def download_page(config: Config, page_name: str) -> Page:
+def download_page(config: Config, io_manager: IOManager, page_name: str) -> Page:
     page = wikia.page(config.wiki_name, page_name)
     html = page.html()
     soup = bs4.BeautifulSoup(html, "html.parser")
 
     outgoing_links = parse_outgoing_links(soup)
 
-    return Page(
-        name=page_name,
-        # html = html,
-        html="",
-        outgoing_links=outgoing_links,
-    )
+    html_path = io_manager.html_writer.write_html(page_name, html)
+
+    return Page(name=page_name, html_path=html_path, outgoing_links=outgoing_links,)
 
 
 def parse_outgoing_links(soup: bs4.BeautifulSoup) -> Set[str]:
